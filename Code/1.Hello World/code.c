559fd3de80576ad9d1820b3f66472e3526d5384d
Mightnire <72493627+Mightnire@users.noreply.github.com>
2020-11-15 16:37:47 +0800

    Update code.c

--- Code/1.Hello World/code.c
+++ Code/1.Hello World/code.c
@@ -1,1 +1,18 @@
+#include "StdAfx.h"
 #include<stdio.h>
+int main()
+{
+  int a,b,x,y;
+  printf("输入生成图形的长和宽：");
+  scanf("%d,%d",&a,&b);
+    for(x=0;x<a;x++)
+    {
+      for(y=0;y<b;y++)
+        {
+          printf("*");
+        }
+      printf("\n");
+    }
+	getchar();
+	getchar();
+}
