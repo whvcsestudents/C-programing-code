26b7d5f972588e7b295d71392fef5f9e73be91a6
Mightnire <72493627+Mightnire@users.noreply.github.com>
2020-11-15 16:39:55 +0800

    Update code.c
    在C++2010测试通过

--- Code/1.Hello World/code.c
+++ Code/1.Hello World/code.c
@@ -5,9 +5,9 @@ int main()
   int a,b,x,y;
   printf("输入生成图形的长和宽：");
   scanf("%d,%d",&a,&b);
-    for(x=0;x<a;x++)
+    for(x=0;x<a;x++)//外循环 绘制竖向换行
     {
-      for(y=0;y<b;y++)
+      for(y=0;y<b;y++)//内循环 绘制横向字符
         {
           printf("*");
         }
